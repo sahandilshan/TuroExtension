27ab19274f88859b7648fcfc91fd6742f844adc8
hasinthaindrajee <hasintha@wso2.com>
2025-06-16 22:36:44 -0500

    Optimizing code

--- components/turo-token-issuer/src/main/java/com/turo/is/tokenIssure/ExtendedJWTTokenIssuer.java
+++ components/turo-token-issuer/src/main/java/com/turo/is/tokenIssure/ExtendedJWTTokenIssuer.java
@@ -18,60 +18,38 @@ package com.turo.is.tokenIssure;
  * under the License.
  */
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
 import com.nimbusds.jose.*;
 import com.nimbusds.jose.util.Base64URL;
-import com.nimbusds.jose.util.JSONArrayUtils;
 import com.nimbusds.jwt.JWTClaimsSet;
-import com.nimbusds.jwt.PlainJWT;
 import com.nimbusds.jwt.SignedJWT;
 import net.minidev.json.JSONArray;
-import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.wso2.carbon.context.PrivilegedCarbonContext;
 import org.wso2.carbon.identity.application.authentication.framework.model.AuthenticatedUser;
 import org.wso2.carbon.identity.core.util.IdentityTenantUtil;
-import org.wso2.carbon.identity.core.util.IdentityUtil;
 import org.wso2.carbon.identity.oauth.common.OAuthConstants;
 import org.wso2.carbon.identity.oauth.common.exception.InvalidOAuthClientException;
 import org.wso2.carbon.identity.oauth.config.OAuthServerConfiguration;
-import org.wso2.carbon.identity.oauth.dao.OAuthAppDO;
 import org.wso2.carbon.identity.oauth2.IdentityOAuth2Exception;
-import org.wso2.carbon.identity.oauth2.OAuth2Constants;
 import org.wso2.carbon.identity.oauth2.authz.OAuthAuthzReqMessageContext;
 import org.wso2.carbon.identity.oauth2.dto.OAuth2AccessTokenReqDTO;
-import org.wso2.carbon.identity.oauth2.internal.OAuth2ServiceComponentHolder;
-import org.wso2.carbon.identity.oauth2.model.RequestParameter;
 import org.wso2.carbon.identity.oauth2.token.JWTTokenIssuer;
 import org.wso2.carbon.identity.oauth2.token.OAuthTokenReqMessageContext;
-import org.wso2.carbon.identity.oauth2.token.bindings.TokenBinding;
-import org.wso2.carbon.identity.oauth2.token.handlers.claims.JWTAccessTokenClaimProvider;
 import org.wso2.carbon.identity.oauth2.util.OAuth2Util;
-import org.wso2.carbon.identity.openidconnect.CustomClaimsCallbackHandler;
-import org.wso2.carbon.identity.openidconnect.util.ClaimHandlerUtil;
 import org.wso2.carbon.user.api.UserRealm;
 import org.wso2.carbon.user.api.UserStoreException;
-import org.wso2.carbon.user.api.UserStoreManager;
 import org.wso2.carbon.user.core.service.RealmService;
 
 import java.security.Key;
 import java.security.cert.Certificate;
 import java.security.interfaces.RSAPrivateKey;
 import java.util.*;
+import java.util.stream.Collectors;
 
-import static org.wso2.carbon.identity.oauth.common.OAuthConstants.RENEW_TOKEN_WITHOUT_REVOKING_EXISTING_ENABLE_CONFIG;
-import static org.wso2.carbon.identity.oauth.common.OAuthConstants.REQUEST_BINDING_TYPE;
-import static org.wso2.carbon.identity.oauth2.util.OAuth2Util.getAuthzRequestContext;
 import static org.wso2.carbon.identity.oauth2.util.OAuth2Util.getPrivateKey;
 
-/**
- * Extended JWT Token Issuer to extend issuing of refresh token in JWT format.
- */
 public class ExtendedJWTTokenIssuer extends JWTTokenIssuer {
 
     private static final String AUTHORIZED_ORGANIZATION_NAME_ATTRIBUTE = "org_name";
@@ -82,127 +60,88 @@ public class ExtendedJWTTokenIssuer extends JWTTokenIssuer {
     private static final String DEFAULT_TYP_HEADER_VALUE = "jwt";
     private static final String JWT_TYP_HEADER_VALUE = "jwt";
     private static final String CUSTOM_TOKEN_ATTR_TURO = "is_signup";
+    private static final String CLAIM_SCOPE = "scope";
+    private static final String CLAIM_AUT = "aut";
+    private static final String CLAIM_AZP = "azp";
+    private static final String ROLE_EVERYONE = "everyone";
+
     private Algorithm signatureAlgorithm = null;
     private static final Log log = LogFactory.getLog(ExtendedJWTTokenIssuer.class);
 
     public ExtendedJWTTokenIssuer() throws IdentityOAuth2Exception {
-
         if (log.isDebugEnabled()) {
-            log.debug("JWT Access token builder is initiated");
+            log.debug("Custom JWT Access token builder is initiated");
         }
-
-        OAuthServerConfiguration config = OAuthServerConfiguration.getInstance();
-
-        // Map signature algorithm from identity.xml to nimbus format, this is a one time configuration.
-        signatureAlgorithm = mapSignatureAlgorithm(config.getSignatureAlgorithm());
+        signatureAlgorithm = mapSignatureAlgorithm(OAuthServerConfiguration.getInstance().getSignatureAlgorithm());
     }
 
-    @Override
-    protected String buildJWTToken(OAuthTokenReqMessageContext request) throws IdentityOAuth2Exception {
-
-        // Set claims to jwt token.
-        JWTClaimsSet jwtClaimsSet = createJWTClaimSet(null, request, request.getOauth2AccessTokenReqDTO()
-                .getClientId());
-        JWTClaimsSet.Builder jwtClaimsSetBuilder = new JWTClaimsSet.Builder(jwtClaimsSet);
-
-        List<JWTAccessTokenClaimProvider> claimProviders = getJWTAccessTokenClaimProviders();
-        for (JWTAccessTokenClaimProvider claimProvider : claimProviders) {
-            Map<String, Object> additionalClaims = claimProvider.getAdditionalClaims(request);
-
-            if (additionalClaims != null) {
-                //remove the claim org_name and org_id if exists
-                additionalClaims.remove(AUTHORIZED_ORGANIZATION_NAME_ATTRIBUTE);
-                additionalClaims.remove(AUTHORIZED_ORGANIZATION_ID_ATTRIBUTE);
-
-                additionalClaims.forEach(jwtClaimsSetBuilder::claim);
-            }
-        }
-
-        OAuth2AccessTokenReqDTO oAuth2AccessTokenReqDTO = request.getOauth2AccessTokenReqDTO();
-        String grantType = oAuth2AccessTokenReqDTO.getGrantType();
-
-        RequestParameter[] requestParametersArray = oAuth2AccessTokenReqDTO.getRequestParameters();
-
-        //adding additional properties in oauth2/token request to JWT.
-        for (RequestParameter requestParameter : requestParametersArray) {
-            String propertyKey = requestParameter.getKey();
-
-            if (CUSTOM_TOKEN_ATTR_TURO.equals(propertyKey)) {
-                jwtClaimsSetBuilder.claim(propertyKey, requestParameter.getValue()[0]);
-            }
-        }
-
-        //fetch user roles and attach as claim authorities if grant type is client credentials
-        if (grantType.equals(GRANT_TYPE_CLIENT_CREDENTIALS)) {
+    private String[] getRolesForClientCredentials(OAuthTokenReqMessageContext request,
+                                                  OAuth2AccessTokenReqDTO dto) throws IdentityOAuth2Exception {
+        if (GRANT_TYPE_CLIENT_CREDENTIALS.equals(dto.getGrantType())) {
             int tenantId = getTenantId(request, null);
-            String appClientId = oAuth2AccessTokenReqDTO.getClientId();
-
-            String[] roles = getUserRoles(tenantId, appClientId);
-            jwtClaimsSetBuilder.claim(AUTHORITIES_ATTRIBUTE, roles);
+            String[] roles = getUserRoles(tenantId, dto.getClientId());
+            return roles;
         }
-
-        removeValues(jwtClaimsSetBuilder);
-        modifyAuthoritiesAndScopes(jwtClaimsSetBuilder);
-        jwtClaimsSet = jwtClaimsSetBuilder.build();
-
-        if (JWSAlgorithm.NONE.getName().equals(signatureAlgorithm.getName())) {
-            return new PlainJWT(jwtClaimsSet).serialize();
-        }
-        return signJWT(jwtClaimsSet, request, null);
+        return new String[0];
     }
 
+    private void addCustomAttributes(OAuth2AccessTokenReqDTO dto, JWTClaimsSet.Builder builder) {
+        Arrays.stream(dto.getRequestParameters())
+                .filter(param -> CUSTOM_TOKEN_ATTR_TURO.equals(param.getKey()))
+                .findFirst()
+                .ifPresent(param -> builder.claim(param.getKey(), param.getValue()[0]));
+    }
 
     private void removeValues(JWTClaimsSet.Builder builder) {
-        builder.issuer(null);
-        builder.subject(null);
-        builder.notBeforeTime(null);
-        builder.audience((String) null);
-        builder.claim("aut", null);
-        builder.claim("azp", null);
+        builder.issuer(null)
+                .subject(null)
+                .notBeforeTime(null)
+                .audience((String) null)
+                .claim(CLAIM_AUT, null)
+                .claim(CLAIM_AZP, null)
+                .claim(AUTHORIZED_ORGANIZATION_ID_ATTRIBUTE, null)
+                .claim(AUTHORIZED_ORGANIZATION_NAME_ATTRIBUTE, null);
     }
 
-    private void modifyAuthoritiesAndScopes(JWTClaimsSet.Builder builder) {
-        JWTClaimsSet claimSet = builder.build();
-        Object scopeObj = claimSet.getClaims().get("scope");
-        if (scopeObj != null) {
-            String scope = scopeObj.toString();
-            if (StringUtils.isEmpty(scope)) {
-                builder.claim("scope", null);
+    private void modifyAuthoritiesAndScopes(JWTClaimsSet.Builder builder, JWTClaimsSet claimsSet, String[] roles) {
+        handleScopeClaim(builder, claimsSet.getClaim(CLAIM_SCOPE));
+        handleAuthoritiesClaim(builder, claimsSet.getClaim(AUTHORITIES_ATTRIBUTE), roles);
+    }
+
+    private void handleScopeClaim(JWTClaimsSet.Builder builder, Object scopeObj) {
+        if (scopeObj instanceof String) {
+            String scopeStr = (String) scopeObj;
+            if (!StringUtils.isEmpty(scopeStr)) {
+                builder.claim(CLAIM_SCOPE, scopeStr.split(" "));
             } else {
-                builder.claim("scope", scope.split(" "));
+                builder.claim(CLAIM_SCOPE, null);
             }
-
         } else {
-            builder.claim("scope", null);
+            builder.claim(CLAIM_SCOPE, null);
         }
-        Object authoritiesObj = claimSet.getClaims().get("authorities");
+    }
 
-        if (authoritiesObj != null) {
-            if (authoritiesObj instanceof JSONArray) {
-                JSONArray authoritiesArray = (JSONArray) authoritiesObj;
-                for (int i = 0; i < authoritiesArray.size(); i++) {
-                    authoritiesArray.set(i, authoritiesArray.get(i).toString().replace("Internal/", ""));
-                }
-                authoritiesArray.remove("everyone");
-            } else if (authoritiesObj instanceof String) {
-                String authority = authoritiesObj.toString();
-                if (authority.contains("Internal/everyone") || authority.contains("everyone")) {
-                    builder.claim("authorities", null);
-                } else {
-                    builder.claim("authorities", authority.replace("Internal/", ""));
-                }
-            } else if (authoritiesObj instanceof String[]) {
-                String[] authString = (String[]) authoritiesObj;
-                List<String> list = new ArrayList<>(Arrays.asList(authString));
-
-                // Remove the specific string
-                list.remove("everyone");
-
-                // Convert list back to array (optional)
-                String[] newArray = list.toArray(new String[0]);
-                builder.claim("authorities", newArray);
-
+    private void handleAuthoritiesClaim(JWTClaimsSet.Builder builder, Object authoritiesObj, String[] roles) {
+        if (authoritiesObj instanceof JSONArray) {
+            JSONArray authoritiesArray = (JSONArray) authoritiesObj;
+            for (int i = 0; i < authoritiesArray.size(); i++) {
+                authoritiesArray.set(i, authoritiesArray.get(i).toString().replace(INTERNAL_ATTRIBUTE, ""));
             }
+            authoritiesArray.remove(ROLE_EVERYONE);
+            builder.claim(AUTHORITIES_ATTRIBUTE, authoritiesArray);
+        } else if (authoritiesObj instanceof String) {
+            String authority = (String) authoritiesObj;
+            if (authority.contains(ROLE_EVERYONE)) {
+                builder.claim(AUTHORITIES_ATTRIBUTE, null);
+            } else {
+                builder.claim(AUTHORITIES_ATTRIBUTE, authority.replace(INTERNAL_ATTRIBUTE, ""));
+            }
+        } else if (roles != null && roles.length > 0) {
+            List<String> filtered = Arrays.stream(roles)
+                    .map(role -> role.replace(INTERNAL_ATTRIBUTE, ""))
+                    .collect(Collectors.toList());
+            filtered.remove(ROLE_EVERYONE);
+            builder.claim(AUTHORITIES_ATTRIBUTE, filtered.toArray(new String[0]));
         }
     }
 
@@ -211,6 +150,7 @@ public class ExtendedJWTTokenIssuer extends JWTTokenIssuer {
                                     OAuthAuthzReqMessageContext authorizationContext) throws IdentityOAuth2Exception {
 
         try {
+            jwtClaimsSet = modifyBeforeSigning(jwtClaimsSet, tokenContext, authorizationContext);
             String tenantDomain = resolveSigningTenantDomain(tokenContext, authorizationContext);
             //int tenantId = IdentityTenantUtil.getTenantId(tenantDomain);
             int tenantId = getTenantId(tokenContext, authorizationContext);
@@ -241,6 +181,18 @@ public class ExtendedJWTTokenIssuer extends JWTTokenIssuer {
         }
     }
 
+    private JWTClaimsSet modifyBeforeSigning(JWTClaimsSet jwtClaimsSet,
+                                             OAuthTokenReqMessageContext tokenContext,
+                                             OAuthAuthzReqMessageContext authorizationContext) throws IdentityOAuth2Exception {
+        OAuth2AccessTokenReqDTO dto = tokenContext.getOauth2AccessTokenReqDTO();
+        JWTClaimsSet.Builder builder = new JWTClaimsSet.Builder(jwtClaimsSet);
+        removeValues(builder);
+        String[] roles = getRolesForClientCredentials(tokenContext, dto);
+        addCustomAttributes(dto, builder);
+        modifyAuthoritiesAndScopes(builder, jwtClaimsSet, roles);
+        return builder.build();
+    }
+
     private String resolveSigningTenantDomain(OAuthTokenReqMessageContext tokenContext,
                                               OAuthAuthzReqMessageContext authorizationContext)
             throws IdentityOAuth2Exception {
@@ -261,11 +213,6 @@ public class ExtendedJWTTokenIssuer extends JWTTokenIssuer {
             throw new IdentityOAuth2Exception("Could not determine the authenticated user and the service provider");
         }
         return getSigningTenantDomain(clientID, authenticatedUser);
-    }
-
-    private static List<JWTAccessTokenClaimProvider> getJWTAccessTokenClaimProviders() {
-
-        return OAuth2ServiceComponentHolder.getInstance().getJWTAccessTokenClaimProviders();
     }
 
     private String getSigningTenantDomain(String clientID, AuthenticatedUser authenticatedUser)
@@ -333,32 +280,17 @@ public class ExtendedJWTTokenIssuer extends JWTTokenIssuer {
     }
 
     private String[] getUserRoles(int tenantId, String username) {
-
         RealmService realmService = (RealmService) PrivilegedCarbonContext.getThreadLocalCarbonContext()
                 .getOSGiService(RealmService.class, null);
-
-        UserRealm userRealm = null;
         try {
-            userRealm = realmService.getTenantUserRealm(tenantId);
-            UserStoreManager userStoreManager = userRealm.getUserStoreManager();
-            String[] roles = userStoreManager.getRoleListOfUser(username);
-            roles = removeInternalFromRoles(roles);
-            return roles;
+            UserRealm userRealm = realmService.getTenantUserRealm(tenantId);
+            return userRealm.getUserStoreManager().getRoleListOfUser(username);
         } catch (UserStoreException e) {
             if (log.isDebugEnabled()) {
-                log.debug("Error while retrieving user roles " + e);
+                log.debug("Error while retrieving user roles", e);
             }
-
             return new String[0];
         }
-    }
-
-    private String[] removeInternalFromRoles(String[] roles) {
-        roles = Arrays.stream(roles)
-                .map(s -> s.startsWith(INTERNAL_ATTRIBUTE) ? s.substring(INTERNAL_ATTRIBUTE.length()) : s)
-                .toArray(String[]::new);
-
-        return roles;
     }
 
     private int getTenantId(OAuthTokenReqMessageContext tokenContext,
